dd6089b538c70929d002aa761b44c31804cb1d0a
cajomaso <casoeder@gmail.com>
2026-04-19 17:30:08 +0000

    Update assignment1.py This file now depicts my first ever ASCII Art Code.

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -1,1 +1,26 @@
+#First I tried the method of doing three quotation marks but the right ear of the cat or dog (that is up for interpretation I originally wanted it to be a Shiba) kept making problems in pycharm by deleting the space right to it.
+print("""   /\_ /\
+  ( •`-´•)
+  /¯¯°¯¯ \/\  (*)
+ _\_\/___/_/_  | .
+ ______________|/__
+ """)
+#Then I used the method of putting a lot of print functions below each other, which made it easy to depict the final art and make changes accordingly. This is also only working because my art is small. For bigger art that is too tedious and I would choose the """ - Method.
+print("                                  ")
+print("   /\_ /\                /\ _/\  ")
+print("  ( •`-´•)              (•`-´• ) ")
+print("  /¯¯°¯¯ \/\   (*)    /\/ ¯¯°¯¯\ ")
+print(" _\_\/___/_/_   | .  _\_\___\/_/  ")
+print("________________|/_______________ ")
+print("___----_-------_--_-------_---____ ")
+print("__-___/ ~~~~    \/         \___-___ ")
+print("____/               ~~~~     \______ ")
+print("____\    ~~~                 /______ ")
+print("__--_\        ~~~      ~~~  /____-__ ")
+print("_______\    ~~~~          /________ ")
+print("_________\      ~~~~    /___----___ ")
+print("___-__--____\        /_________-__ ")
+print("_-___-________\  ~ /____---_______ ")
+print("___--___-_______\/____________ ")
+print("_____-________________--_____ ")
 
